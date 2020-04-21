80157ba306757276cb892d3e8e94d0952f6a441f
sanjeet kumar <sanjeet.kumar2@delhivery.com>
2020-04-21 21:41:49 +0530

    Updated games with dynamic through api

--- src/App.js
+++ src/App.js
@@ -2,27 +2,97 @@ import React from "react";
 import "./styles.css";
 import { Header } from "./components/header.component";
 import { GamesList } from "./components/games-list.component";
+const years = ["2020","2019","2018","2017","2016","2015","2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000"]
 
 class App extends React.Component {
   constructor(props) {
     super();
     this.state = {
       gamesList: [],
-      isLoading: true
+      isLoading: true,
+      apiData:[]
     };
   }
 
-  fetchGames() {
-    fetch(`http://starlord.hackerearth.com/gamesext`, {
-      headers: {
-        "Access-Control-Allow-Origin": "*"
+   filterList = (event) => {
+     let gamesList = this.state.apiData;
+     let updatedGamelist = gamesList.filter((item) => {
+       return item.title.toString().toLowerCase().includes(event.target.value.toLowerCase())
+     });
+     this.setState({
+       gamesList: updatedGamelist
+     });
+
+     if (!event.target.value) {
+         this.setState({
+           gamesList: this.state.apiData
+         });
+     }
+   }
+   updateYear = (event) => {
+      let gamesList = this.state.apiData;
+      let updatedGamelist = gamesList.filter((item) => {
+        return item.release_year == (event.target.value)
+      });
+
+      this.setState({
+        gamesList: updatedGamelist
+      });
+
+      if (!event.target.value) {
+        this.setState({
+          gamesList: this.state.apiData
+        });
       }
-    })
+   }
+    updateYear = (event) => {
+      let gamesList = this.state.apiData;
+      let updatedGamelist = gamesList.filter((item) => {
+        return item.release_year == (event.target.value)
+      });
+
+      this.setState({
+        gamesList: updatedGamelist
+      });
+
+      if (!event.target.value) {
+        this.setState({
+          gamesList: this.state.apiData
+        });
+      }
+   }
+    
+   scoreSort = (event) => {
+      let gamesList = this.state.apiData;
+       let updatedGamelist;
+      if (event.target.value === '0') {
+        updatedGamelist = gamesList.sort((a, b) => a.score - b.score)
+      }
+
+      if (event.target.value === '1') {
+        updatedGamelist = gamesList.sort((a, b) => a - b).reverse();
+      }
+     
+
+      this.setState({
+        gamesList: updatedGamelist
+      });
+
+      if (!event.target.value) {
+        this.setState({
+          gamesList: this.state.apiData
+        });
+      }
+   }
+
+  fetchGames() {
+    fetch(`http://starlord.hackerearth.com/gamesext`)
       .then(response => response.json())
       .then(data =>
         this.setState({
           gamesList: data,
-          isLoading: false
+          isLoading: false,
+          apiData:data
         })
       )
       // Catch any errors we hit and update the app
@@ -37,8 +107,29 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
+         <div className="form-group">
+          <input type="text" className="search-game" placeholder="Search" onChange={this.filterList}/>
+          <div>
+            <span>Sort by Year: </span> 
+            <select onChange={this.updateYear}>
+                  <option value="" selected>select</option>
+              {
+                years.map(year => (
+                <option value={year}>{year}</option>
+                ))
+              }
+          </select>
+          </div>
+            <div>
+            <span>Sort by score: </span> 
+            <select onChange={this.scoreSort}>
+                  <option value="" selected>select</option>
+                  <option value="0">Accending</option>
+                  <option value="1">Decending</option>
+          </select>
+          </div>
+        </div>
         <GamesList gameList={this.state.gamesList} />
-        <h2>Start editing to see some magic happes</h2>
       </div>
     );
   }
--- src/components/games-list.component.js
+++ src/components/games-list.component.js
@@ -1,10 +1,46 @@
 import React from "react";
 
-export const GamesList = props => {
-  console.log(props);
+export const GamesList = (props) => {
+  const { gameList } = props
   return (
-    <div className="card">
-      <h1>game</h1>
+    <div className="games-list">
+      {
+      gameList.map((user, i) => {
+            const {
+              title,
+              url,
+              platform,
+              score,
+              genre,
+              editors_choice,
+              release_year
+             } = user;
+            return (
+              <div key={i} className="card">
+                  <p className="score">{score}</p>
+                   <p className="year">{release_year}</p>
+                 <div>
+                  <img src={`https://robohash.org/san` +genre } alt={genre} />
+                </div>
+              <p  className="title">{title}</p>
+                <p className="platform">{platform}</p>
+                <a href={url}>View Game</a>
+                { editors_choice ? (<i className="fa fa-star" aria-hidden="true"></i>): '' }
+               
+              </div>
+            );
+          })
+          }
     </div>
   );
 };
+
+
+// title: "LittleBigPlanet PS Vita",
+//   url: "/games/littlebigplanet-vita/vita-98907",
+//   platform: "PlayStation Vita",
+//   score: 9,
+//   genre: "Platformer",
+//   editors_choice: "Y",
+//   release_year: 2012
+// },
